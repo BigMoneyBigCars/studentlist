fff56ec2a63969d8d30b6190d36b79fcc03f214f
Lasse <L.stausgaard@gmail.com>
2020-02-09 18:01:51 +0100

    both dropdown and theme on popup by click works

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ let students;
 function startpage() {
   console.log("test");
 
+  theme();
+
   getJson();
 }
 
@@ -26,20 +28,30 @@ function showStudents() {
     klon.querySelector("h2").textContent = element.fullname;
     klon.querySelector("p").textContent = element.house;
 
+    klon.querySelector(".singleProdukt").dataset.theme = element.house;
     klon.querySelector(".singleProdukt").addEventListener("click", function() {
+      document.querySelector("#popUp").dataset.theme = element.house;
       document.querySelector("#popUp h2").textContent = element.fullname;
       document.querySelector("#popUp p").textContent = element.house;
 
       document.querySelector("#popUp").style.display = "block";
       document.querySelector(".luk").addEventListener("click", noDisplayPopUp);
-      document
-        .querySelector("#popUp")
-        .addEventListener("click", noDisplayPopUp);
+      document.querySelector("#popUp").addEventListener("click", noDisplayPopUp);
     });
     liste.appendChild(klon);
   });
 }
-
+// pop up
 function noDisplayPopUp() {
   document.querySelector("#popUp").style.display = "none";
+}
+
+function theme() {
+  document.querySelector("select#theme").addEventListener("change", selected);
+
+  function selected() {
+    const selectedTheme = this.value;
+    console.log(selectedTheme);
+    document.querySelector("section").dataset.theme = selectedTheme;
+  }
 }
