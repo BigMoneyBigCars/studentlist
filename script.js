e417673b44d8ade408d76a2c8d0f417e8a0ca89e
Lasse <L.stausgaard@gmail.com>
2020-02-20 22:07:21 +0100

    array strings done except for nickname ernie

--- script.js
+++ script.js
@@ -1,23 +1,93 @@
-window.addEventListener("DOMContentLoaded", startpage);
-let students;
-function startpage() {
+window.addEventListener("DOMContentLoaded", start);
+const allStudents = [];
+
+function start() {
+  console.log("ready");
+
+  loadJSON();
+}
+
+function loadJSON() {
+  fetch("https://petlatkea.dk/2020/hogwarts/students.json")
+    .then(response => response.json())
+    .then(jsonData => {
+      // when loaded, prepare objects
+      prepareObjects(jsonData);
+    });
+}
+
+const Student = {
+  firstname: "",
+  lastname: "unknown",
+  middlename: "",
+  house: "",
+  nickname: "",
+  image: "",
+  age: 0
+};
+
+function prepareObjects(jsonData) {
   console.log("test");
+  jsonData.forEach(jsonObject => {
+    const student = Object.create(Student);
 
-  theme();
+    let fullName = jsonObject.fullname.trim();
+    fullName = fullName.toLowerCase();
 
-  getJson();
+    let firstSpace = fullName.indexOf(" ");
+    let firstName = fullName.substring(0, firstSpace);
+
+    let lastSpace = fullName.lastIndexOf(" ");
+    let lastName = fullName.substring(lastSpace + 1, fullName.length);
+
+    let middleName = fullName.substring(firstSpace + 1, lastSpace);
+
+    //  if (middleName == middleName) {
+    //} else middleName = " " + middleName + " ";
+
+    let find = middleName.substring(0, 1);
+
+    //uppercase/*
+    firstName = firstName.charAt(0).toUpperCase() + firstName.substring(1);
+
+    lastName = lastName.charAt(0).toUpperCase() + lastName.substring(1);
+
+    if (middleName.length <= 1) {
+    } else {
+      middleName = " " + middleName.charAt(0).toUpperCase() + middleName.substring(1) + " ";
+    }
+
+    //middleName = middleName.charAt(0).toUpperCase() + middleName.substring(1);
+
+    if (find === `"`) {
+      student.nickName = middleName;
+      console.log(student.nickName.trim()); // trim for nickname, remember to capitalize
+    } else {
+      middleName = middleName;
+    }
+
+    console.log(firstName + middleName + lastName);
+
+    //house
+
+    let house = jsonObject.house.trim();
+
+    house = house.toLowerCase();
+
+    let upperHouse = house.charAt(0).toUpperCase() + house.substring(1);
+
+    console.log(upperHouse);
+  });
+
+  displayList();
 }
 
-async function getJson() {
-  let jsonData = await fetch("students1991.json");
+function displayList() {
+  console.log("Display list objects function");
 
-  students = await jsonData.json();
-
-  console.log(students);
-
-  showStudents();
+  console.log("Hej Victoria");
 }
-
+/* 
 function showStudents() {
   const liste = document.querySelector("#student-grid");
   const skabelon = document.querySelector(".template");
@@ -55,3 +125,4 @@ function theme() {
     document.querySelector("section").dataset.theme = selectedTheme;
   }
 }
+ */
